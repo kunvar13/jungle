697ae065416a6c8fd00eb54967b9c00ae6dc5ad8
kunvar13 <kunvar13@gmail.com>
2022-01-06 11:35:27 -0600

    Added dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   def show
+    @products = Product.order(id: :desc).all
+    @category = Category.all
   end
 end

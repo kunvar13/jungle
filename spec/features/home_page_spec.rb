74a2d66e6470bdbe61666906a161bc6d52f29bf8
kunvar13 <kunvar13@gmail.com>
2022-01-11 21:53:22 -0600

    Created product details spec file

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -24,6 +24,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # DEBUG / VERIFY
     save_screenshot
 
+    # DEBUG / VERIFY
     expect(page).to have_css 'article.product', count: 10
   end
 
